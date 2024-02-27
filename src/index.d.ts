be21a497b53ea6237645424be9ba5344e9e11b36
Uzair Ansari <124576222+UzairAnsari054@users.noreply.github.com>
2024-02-27 12:58:52 +0530

    FInal Push

---
+++ src/index.d.ts
@@ -0,0 +1,11 @@
+interface Product {
+    id: string
+    name: string
+    imageUrl: string
+    originalPrice: number
+    originalPrice: number
+    offerPercentage: number
+    rating: number
+    ratingCount: number
+    tags: string[]
+}
